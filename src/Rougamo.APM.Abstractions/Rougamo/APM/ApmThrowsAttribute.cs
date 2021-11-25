376b70cee294dcbdff7dcd18ab358af4936e3ce6
ihourglass <inversionhourglass@outlook.com>
2021-11-26 02:13:17 +0800

    add ApmThrowsAttribute

---
+++ src/Rougamo.APM.Abstractions/Rougamo/APM/ApmThrowsAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rougamo.APM
+{
+    /// <summary>
+    /// do not mute the exception even it has been recorded
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ApmThrowsAttribute : Attribute
+    {
+    }
+}
